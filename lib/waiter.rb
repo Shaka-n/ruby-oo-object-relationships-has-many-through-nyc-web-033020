8e9ddefcc6f147301c318eb5d67a7c79aee90687
Garen Sahagian <g.s.sahagian@gmail.com>
2020-04-03 16:01:49 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,7 +20,12 @@ class Waiter
     end
 
     def best_tipper
-
+        tips = meals.map {|meal| meal.tip}
+        high_tip = tips.max
+        high_meal = meals.find {|meal| meal.tip==high_tip}
+        high_meal.customer
     end
+        
+        
 
 end
