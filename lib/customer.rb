cf0cfa0a5a130932e391a0ed296a39fbac7051d7
Garen Sahagian <g.s.sahagian@gmail.com>
2020-04-03 10:32:46 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,25 @@
 class Customer
-  
+    @@all=[]
+    attr_accessor :total, :tip, :waiter
+  def initialize(name,age)
+    @name=name
+    @age=age
+    Customer.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    meals = Meal.all.select {|meal| meal.customer ==self}
+  end
+
+  def waiters
+    meals.map {|meal| meal.waiter}
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
 
+    @@all = []
+    def initialize(name, exp)
+        @name = name
+        @exp =exp
+        Waiter.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter==self}
+    end
+
+    def best_tipper
+
+    end
+
 end
